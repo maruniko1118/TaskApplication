031fcc1c2816b3077eb7259d31868df1a1c02e36
maaaaaaru2525 <116351811+maaaaaaru2525@users.noreply.github.com>
2023-12-13 20:38:47 +0900

    Changes config button position

--- app/src/main/java/com/maru2525/taskapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/maru2525/taskapplication/ui/activity/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.maru2525.taskapplication.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.maru2525.taskapplication.R
 import com.maru2525.taskapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,10 +31,26 @@ class MainActivity : AppCompatActivity() {
     // menu should be considered as top level destinations.
     val appBarConfiguration = AppBarConfiguration(
       setOf(
-        R.id.navigation_task, R.id.navigation_archive, R.id.navigation_config
+        R.id.navigation_task, R.id.navigation_archive
       )
     )
     setupActionBarWithNavController(navController, appBarConfiguration)
     navView.setupWithNavController(navController)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_main, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      R.id.menu_button -> {
+        val intent = Intent(this, ConfigActivity::class.java)
+        startActivity(intent)
+        return true
+      }
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
